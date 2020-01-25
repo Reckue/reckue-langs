2be4a82547181e04216c3e4c54c2413d6338c804
Hardelele <i.lapshina@mail.ru>
2020-01-25 21:20:07 +0300

    Add base note-api models and enums

---
+++ src/com/reckure/util/enums/ContentType.java
@@ -0,0 +1,34 @@
+package com.reckure.util.enums;
+
+public enum ContentType {
+
+    /**
+     * Messages
+     */
+    TEXT,
+
+    /**
+     * Picture
+     */
+    JPG, GIF, BMP, TIFF, PNG,
+
+    /**
+     * Audio
+     */
+    MP3, WMA,
+
+    /**
+     * Video
+     */
+    MP4, AVI, WMF, MPG2,
+
+    /**
+     * Document
+     */
+    TXT, DOC, RTF, DOCX, XLS, XLSX, XLSM, ODS, RAR, ZIP, TG,
+
+    /**
+     * Undefined formats
+     */
+    UNDEFINED
+}
---
+++ src/com/reckure/util/enums/ModelType.java
@@ -0,0 +1,7 @@
+package com.reckure.util.enums;
+
+public enum ModelType {
+    NOTE,
+    CONTENT,
+    HISTORY
+}
