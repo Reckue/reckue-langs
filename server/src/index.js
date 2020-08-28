97f6cdbd705555943bc29d402c1c6f1b9c48d190
Hardelele <Hardelele@yahoo.com>
2020-08-28 23:21:21 +0300

    Add mongo dependency

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
 const app = express();
 const port = 3000;
 
