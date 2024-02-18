34645a202ad963a175295ce12fe888a71e35d241
Viktor <Vikkingg13@gmail.com>
2024-02-18 16:39:55 +0300

    Add TODO

--- src/page/block/managers/PageManager.ts
+++ src/page/block/managers/PageManager.ts
@@ -2,7 +2,7 @@ import {BlockHighlighting} from "../../realtime/highlighting/BlockHighlighting";
 import {CacheManager} from "./CacheManager";
 import {PopupManager} from "./PopupManager";
 import {CacheModel} from "./models/CacheModel";
-import {ParserService} from "../../realtime/parser/ParserService";
+import {ParserService} from "../../realtime/parser/services/ParserService";
 
 
 export class PageManager {
@@ -24,12 +24,16 @@ export class PageManager {
     onclick = (event: MouseEvent) => {
         let cache: CacheModel = this.cacheManager.getCache(event);
         if (cache) {
+            //TODO Move to constructor
             const parser = new ParserService(event, cache.textBlocks);
-            // const word = parser.getWord();
+            //TODO Create BlockMetaService
+            const word = parser.getWord(
+                //TODO Provide BlockMetaModel 
+            );
 
-            const netGraph = parser.getNetGraph();
+            const netGraph = parser.getNetGraphProperties();
 
-            // this.#popupManager.updatePopup(word, netGraph);
+            this.popupManager.updatePopup(word, netGraph);
         }
     }
 
